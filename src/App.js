8b4f92d834cf4a2824598f20d858863483158c8d
rajiv <rajvirat180@gmail.com>
2021-06-08 20:01:27 +0530

    2.About Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>film</h1>
+      <AboutUs />
     </div>
   );
 }
 
 export default App;
-
