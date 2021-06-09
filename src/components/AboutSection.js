7c85a8602985bccf21d6067ab619e569b5a42bec
rajiv <rajvirat180@gmail.com>
2021-06-09 20:46:47 +0530

    6.intro to styled pt 2

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -34,35 +34,5 @@ const AboutSection = () => {
 };
 
 //styled components
-const StyledAbout = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-  min-height: 90vh;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
